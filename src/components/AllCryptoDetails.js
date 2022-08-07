c8cab24cd570b6b240cff8ed830df87689aa21c3
whit33y <troppout@gmail.com>
2022-08-07 15:39:56 +0200

    Changes in cryptodetails

--- src/components/AllCryptoDetails.js
+++ src/components/AllCryptoDetails.js
@@ -26,21 +26,25 @@ function AllCryptoDetails() {
     return (
         <div className='container'>
             <Navbar />
+            <button><a href='/crypto'>Back to cryptos</a></button>
             <div className='showCryptoHeader'>
                 <img src={`${cryptoData.image.large}`} className='showCryptoImg'/>
                 <h2>{cryptoData.name} - {cryptoData.symbol}</h2>
+                <h6>Market rank: {cryptoData.market_cap_rank}</h6>
             </div>
             <div className='showCryptoContent'>
                 <p>Highest price(24h): {cryptoData.market_data.high_24h.usd} USD <br />Lowest price(24h): {cryptoData.market_data.low_24h.usd} USD <br /> Price change(24h): {Math.floor(cryptoData.market_data.price_change_24h_in_currency.usd * 1000)/1000} USD</p>
                 <b><p>Current price in different currencies</p></b> 
                 <p>🇺🇸 {cryptoData.market_data.current_price.usd}usd 🇵🇱 {cryptoData.market_data.current_price.pln}pln 🇪🇺 {cryptoData.market_data.current_price.eur}eur 🇯🇵 {cryptoData.market_data.current_price.jpy}jpy<br/> 🇨🇳{cryptoData.market_data.current_price.cny}cny 🇬🇧{cryptoData.market_data.current_price.gbp}gbp 🇦🇺{cryptoData.market_data.current_price.aud}aud🇨🇭{cryptoData.market_data.current_price.chf}chf</p>
-                {/* <b><p>Popularity rate:</p></b> 
-                <p>👍{Math.floor(cryptoData.sentiment_votes_up_percentage* 10)/10} 👎 {Math.ceil(cryptoData.sentiment_votes_down_percentage* 10)/10}</p> */}
+                <p>All time highest price: {cryptoData.market_data.ath.usd} usd 📈<br />All time lowest price: {cryptoData.market_data.atl.usd} usd 📉</p>
                 <div className='showCryptoLinks'>
                 <a href={cryptoData.links.homepage[0]}>Visit official {cryptoData.name} website <img src={`${cryptoData.image.thumb}`}/></a>
                 <br />
                 <a href={cryptoData.links.official_forum_url[0]}>Visit official {cryptoData.name} community forum <img src={`${cryptoData.image.thumb}`}/></a>
                 </div>
+            <div className='showCryptoFooter'>
+                <h6>Last update: {cryptoData.last_updated}</h6>
+            </div>
             </div>
             <Footer />
         </div>
--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -6,12 +6,12 @@ function CryptoDetails(props) {
             <p><b>{props.symbol.toUpperCase()}: </b> {props.current_price} $</p>
             <ul>
                 <li><b>Low/High(24h):</b> {props.lowest_24h}$ / {props.highest_24h}$</li>
-                <li><b>Price change(24h/$/%):</b> {Math.floor(props.price_change_24h * 1000)/1000}$ / {Math.floor(props.price_change_percentage_24h*100)/100} %</li>
-                <li><b>All time high:</b> {props.ath}$</li>
-                <li><b>All time low:</b> {props.atl}$</li>
+                <li><b>Price change(24h):</b> {Math.floor(props.price_change_24h * 1000)/1000}$ </li>
+                <li><b>Price change(24h/%):</b> {Math.floor(props.price_change_percentage_24h*1000)/1000} % </li>
+                {/* <li><b>All time high:</b> {props.ath}$</li>
+                <li><b>All time low:</b> {props.atl}$</li> */}
             </ul>
-            <Link to={`/alldetails?q=${props.id}`}>View even more</Link>
-            <p className='cryptoDetailsUpdate'><b>Last updated:</b> {props.last_updated}</p>
+            <Link to={`/alldetails?q=${props.id}`}>View  more</Link>
         </div>
     );
 }
