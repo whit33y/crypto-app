70cb50f085f0b7fc86fccf1a3dc31a049b1f33a0
whit33y <troppout@gmail.com>
2022-08-16 21:05:39 +0200

    searching fix

--- src/components/ShowCrypto.js
+++ src/components/ShowCrypto.js
@@ -9,13 +9,11 @@ function ShowCrypto(props) {
     }
     
     return (
-    <div className="flex flex-col mb-10">
-    <div className={ props.searched_cryptos.includes(props.name.toLowerCase()) ? 'block' : 'hidden'}>
+    <div className={ props.searched_cryptos.includes(props.name.toLowerCase()) ? 'block flex flex-col mb-10' : 'hidden'}>
         <img src={`${props.image}`} className='h-30 w-40 mx-auto'/>
         <h3 className='text-center text-3xl'>{props.name}</h3>
     <div className="flex justify-center">
         <button className='bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full text-1xl' onClick={()=>handleDetails()}><b>{details ? 'HIDE DETAILS' : 'MORE DETAILS'}</b></button>
-    </div>
     </div>
         {details ? <CryptoDetails current_price={props.current_price} highest_24h={props.highest_24h} lowest_24h={props.lowest_24h} symbol={props.symbol} ath={props.ath} atl={props.atl} ath_date={props.ath_date} price_change_24h={props.price_change_24h} price_change_percentage_24h={props.price_change_percentage_24h} last_updated={props.last_updated} id={props.id}/> : ''}
     </div> 
