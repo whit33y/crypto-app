bc03345bab3102ea14ffee214c08164183b2816d
whit33y <troppout@gmail.com>
2022-08-19 10:36:52 +0200

    Changes in searching, new navbar

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -6,16 +6,18 @@ import Navbar from '../layouts/Navbar'
 import Footer from '../layouts/Footer'
 
 function Crypto() {
+    const [search, setSearch] = useState('')
+    const [cryptos, setCryptos] = useState(null)
+    const [searchedCryptos, setSearchedCryptos] = useState()
 
     function searchEngine(cryptoName, searchContent, crypto_array){
         if(cryptoName.includes(searchContent)){
+            // cosnt newArray = [...crypto_array, cryptoName]
             crypto_array.push(cryptoName)
         }
     }
 
-    const [cryptos, setCryptos] = useState(null)
-    const [search, setSearch] = useState('')
-    const [searchedCryptos, setSearchedCryptos] = useState()
+    // console.log(cryptos)
 
     const cryptoNames = []
     let findedCryptos = []
@@ -23,17 +25,28 @@ function Crypto() {
     useEffect(()=>{
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
         .then((res)=> res.json())
-        .then((data)=>setCryptos(data))
+        .then((data)=>{setCryptos(data)})
         },[])
 
-    useEffect(()=>{
-        findedCryptos = []
-        for(let i=0; i<cryptoNames.length; i++){
-            searchEngine(cryptoNames[i], search, findedCryptos)
-        }
-        setSearchedCryptos(findedCryptos)
+    useEffect(() => {
+        const filteredCryptos = cryptos && cryptos.filter(crypto => {
+            const searchedName = crypto.name.toLowerCase()
+            return search === "" ? cryptos : searchedName.includes(search)
+        })
+        setSearchedCryptos(filteredCryptos)
+    }, [search])
 
-    },[search])
+    console.log(searchedCryptos)
+
+
+    // useEffect(()=>{
+    //     findedCryptos = []
+    //     for(let i=0; i<cryptoNames.length; i++){
+    //         searchEngine(cryptoNames[i], search, findedCryptos)
+    //     }
+    //     setSearchedCryptos(findedCryptos)
+
+    // },[search])
 
     if(!cryptos)
     {
@@ -56,19 +69,21 @@ function Crypto() {
                     <input className="w-half p-3 mt-1 text-sm border-2 border-gray-200 rounded" id='input-search' type='text' onChange={event=> setSearch(event.target.value.toLowerCase())} placeholder='search for crypto...'/>
                 </div>
                 </form>
-        {cryptos  && cryptos.map(data=>{
+                <main className="flex gap-20 justify-between flex-wrap">
+        {searchedCryptos  && searchedCryptos.map(data=>{
                 return <ShowCrypto 
                 name={data.name} current_price={data.current_price} image={data.image} 
                 highest_24h={data.high_24h}lowest_24h={data.low_24h} price_change_percentage_24h={data.price_change_percentage_24h} symbol={data.symbol} ath={data.ath} atl={data.atl}
                 ath_date={data.ath_date} price_change_24h={data.price_change_24h} last_updated={data.last_updated} id={data.id} searched_cryptos={searchedCryptos}/>
 
         })} 
+        </main>
         </div>
 
         <Footer />
-        {cryptos  && cryptos.map(data=>{
+        {/* {searchedCryptos  && searchedCryptos.map(data=>{
             cryptoNames.push(data.name.toLowerCase())
-        })} 
+        })}  */}
         </div>
      )
 }
--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -13,32 +13,54 @@ function Navbar() {
     } 
     return (
         <div className='container mx-auto mb-3'>
-        <nav className='p-4 bg-blue-300 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-blue-300 '>
-           <ul className='flex justify-between'>
-            <div className='flex justify-between text-slate-50	'>
-                <li className='mr-10'> 
-                    <Link to='/' className='text-2xl hover:underline'>Home <span className="material-symbols-outlined">home</span></Link>
-                </li>
-                {user ?               
-                <li className='mr-10'>
-                    <Link to='/crypto' className='text-2xl hover:underline'>Cryptos <span className="material-symbols-outlined">currency_bitcoin</span></Link> 
-                </li>      
-                : ''}  
-                <li className='mr-10'>
-                    <Link to='/about' className='text-2xl hover:underline'>About <span className="material-symbols-outlined">explore</span></Link>
-                </li>  
-            </div>
-                {
-                user ? 
-                <li>
-                    <button onClick={handleSignOut} className='text-2xl text-slate-50 hover:underline' >Logout <span className="material-symbols-outlined">logout</span></button>
-                </li>  :
-                <li>
-                    <Link to='/login' className='text-2xl text-slate-50 hover:underline' >Log in <span className="material-symbols-outlined">login</span></Link>
-                </li>
-                }          
-            </ul> 
+    <header class="bg-blue-300 rounded">
+  <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+      <div class="flex-1 md:flex md:items-center md:gap-12">
+        <a class="block text-white s:hidden text-xl font-bold   " href="/">
+          <span class="sr-only">Home</span>
+          Crypto app ™️
+        </a>
+      </div>
+
+      <div class="md:flex md:items-center md:gap-12 mr-5">
+        <nav class=" md:block" aria-labelledby="header-navigation">
+          <h2 class="sr-only" id="header-navigation">Header navigation</h2>
+
+          <ul class="flex items-center gap-6 text-sm">
+            <li>
+              <a class="text-white transition hover:text-white/75" href="/crypto">
+                Cryptos
+              </a>
+            </li>
+
+            <li>
+              <a class="text-white transition hover:text-white/75" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
         </nav>
+        </div>
+        <div class="flex items-center gap-4">
+          <div class="sm:gap-4 sm:flex">
+            { user ?
+             <button onClick={handleSignOut} className='px-5 py-2.5 text-sm font-medium text-black bg-white rounded-md shadow' >Logout</button>
+              :
+            <a
+              class="px-5 py-2.5 text-sm font-medium text-black bg-white rounded-md shadow"
+              href="/login"
+            >
+              Login
+            </a>}
+          </div>
+
+        </div>
+      
+    </div>
+  </div>
+  </header>
+
         <h1 className='text-5xl font-bold sm:text-4xl text-center mt-10'>Crypto app ™️</h1>
         </div>
     );
