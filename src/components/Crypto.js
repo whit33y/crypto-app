cc55a59c210e3f0aa262656446fcc62457915fd2
whit33y <troppout@gmail.com>
2022-08-19 11:39:46 +0200

    searching fix

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -25,7 +25,7 @@ function Crypto() {
     useEffect(()=>{
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
         .then((res)=> res.json())
-        .then((data)=>{setCryptos(data)})
+        .then((data)=>{setCryptos(data);setSearchedCryptos(data)})
         },[])
 
     useEffect(() => {
@@ -33,7 +33,8 @@ function Crypto() {
             const searchedName = crypto.name.toLowerCase()
             return search === "" ? cryptos : searchedName.includes(search)
         })
-        setSearchedCryptos(filteredCryptos)
+
+            setSearchedCryptos(filteredCryptos)
     }, [search])
 
     console.log(searchedCryptos)
